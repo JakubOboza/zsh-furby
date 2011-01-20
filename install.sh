e86253c25d9fb6bdb345232b9706208bcb01d69f
JO <jakub.oboza@gmail.com>
2011-01-20 19:52:25 +0100

    added ZSH PATH to install script

--- install.sh
+++ install.sh
@@ -12,6 +12,19 @@ ln -s `pwd`/zsh/zshrc ~/.zshrc
 print "Linking emacs to ~/.emacs"
 ln -s `pwd`/emacs/emacs ~/.emacs
 
+if [[ $ZSH == "" ]]; then
+  print "You don't have set \$ZSH to any dir"
+  print "Backuping .profile to .profile.old"
+  cp ~/.profile ~/.profile.old
+  print "Adding export ZSH="`pwd`" to your .profile file"
+  echo "export ZSH="`pwd` >> ~/.profile
+  source ~/.profile
+elif [[ $ZSH == `pwd` ]]; then
+  print "Your \$ZSH points to right directory"
+else
+  print "Your \$ZSH is set to:"$ZSH
+  print "Please check if it is pointing to "`pwd`
+fi
 
 
 
